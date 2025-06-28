75a7a0a7148685564be8acca37a9ab8405535a08
SiAn20 <sidneyangellysg@gmail.com>
2025-06-28 13:13:48 -0400

    feat(WarmUp1): Resolve exercise C_Montanas

--- WarmUp1/C_Montañas.cpp
+++ WarmUp1/C_Montañas.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 void solve(){
+    int pos=1, posMax, altura=0, alturaMax=0;
+    string s;
+    cin >> s;
+
+    for(int i=0; i<s.size(); i++){
+        if(s[i] == '+'){
+            altura++;
+        }else{
+            altura--;
+        }
+        if(altura > alturaMax){
+            alturaMax = altura;
+            posMax = pos;
+        }
+        pos++;
+    }
     
+    cout << posMax << endl;
 }
 
 signed main() {
@@ -10,9 +27,7 @@ signed main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t;
-    cin >> t;
-    while (t--) solve();
+    solve();
 }
 
 //by Sidney Angelly Sahonero Garrado
