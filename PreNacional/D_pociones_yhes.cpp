5091b3d169ce698bca517269583beaa3d957e786
SiAn20 <sidneyangellysg@gmail.com>
2025-06-29 18:02:55 -0400

    feat(PreNacional): Resolve exercises D

--- PreNacional/D_pociones_yhes.cpp
+++ PreNacional/D_pociones_yhes.cpp
@@ -1,22 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 
-void solve(){
+int main() {
     int N, M;
     cin >> N >> M;
 
-    while (M--){
-        
+    vector<int> explosivas;
+    // Leer combinaciones explosivas y convertirlas en bitmasks
+    for (int i = 0; i < M; ++i) {
+        int K;
+        cin >> K;
+        int mask = 0;
+        for (int j = 0; j < K; ++j) {
+            int e;
+            cin >> e;
+            mask |= (1 << (e - 1));
+        }
+        explosivas.push_back(mask);
     }
-}
-
-signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    solve();
+    int totalValidas = 0;
+    // Recorrer todos los subconjuntos posibles (desde 0 hasta 2^N - 1)
+    for (int s = 0; s < (1 << N); ++s) {
+        bool esValido = true;
+        for (int maskExplosiva : explosivas) {
+            // Si el subconjunto incluye todos los elementos de una explosiva => inválido
+            if ((s & maskExplosiva) == maskExplosiva) {
+                esValido = false;
+                break;
+            }
+        }
+        if (esValido) {
+            totalValidas++;
+        }
+    }
+    cout << totalValidas << endl;
+    return 0;
 }
 
 //by Sidney Angelly Sahonero Garrado
--- PreNacional/I_conmebol.cpp
+++ PreNacional/I_conmebol.cpp
@@ -65,3 +65,5 @@ int main(){
 
     return 0;
 }
+
+//by Sidney Angelly Sahonero Garrado
