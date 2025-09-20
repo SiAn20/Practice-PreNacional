20f4a2deb210b26b76ebee85e700a8ed762eb010
SiAn20 <sidneyangellysg@gmail.com>
2025-09-20 18:22:57 -0400

    feat: add Hello World program in C++

---
+++ Codificadas/Sept_20_2025/A_Hello_nombre_Hello_nome.cpp
@@ -0,0 +1,28 @@
+/*
+  author: SiAn20
+  Name: Sidney Angelly Sahonero Garrado
+*/
+#include <bits/stdc++.h>
+using namespace std;
+// #define int long long
+#define endl '\n'
+
+void solve() {
+  string s;
+  cin >> s;
+  cout << "Hello " << s << "!" << endl;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+}
+
+/*
+     /\_/\
+    ( o.o )
+     > ^ <
+*/
